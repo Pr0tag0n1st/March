844ab8fe10f3c3416d2a1a032eea2568448d923e
Game110AM <Game110AM@605-110-STU03>
2017-03-21 08:53:59 -0600

    stuff

--- Mar16/Mar16/Randomdata.cpp
+++ Mar16/Mar16/Randomdata.cpp
@@ -1,2 +1,15 @@
 #include <iostream>
 using namespace std;
+#include <cstdlib>
+#include <ctime>
+int data[100];
+void random();
+int main() {
+	srand(static_cast<unsigned int>(time(0)));
+
+
+}
+void random() {
+	for (int i = 1; i < 100; i++) 
+		int data[i] = rand() % 1000 + 1;
+}
