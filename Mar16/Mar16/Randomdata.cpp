bc82302821b870bd684071a523c341a15ffbe11c
Game110AM <Game110AM@605-110-STU03>
2017-03-22 08:23:01 -0600

    New random stuff

--- Mar16/Mar16/Randomdata.cpp
+++ Mar16/Mar16/Randomdata.cpp
@@ -2,14 +2,21 @@
 using namespace std;
 #include <cstdlib>
 #include <ctime>
-int data[100];
+int sum = 0;
+int max = 0;
+int slot = 0;
 void random();
 int main() {
 	srand(static_cast<unsigned int>(time(0)));
-
+	random();
+	cout << "According to our data, the average was " << sum / 100 << " and the max was " << max << "." << endl;
 
 }
 void random() {
-	for (int i = 1; i < 100; i++) 
-		int data[i] = rand() % 1000 + 1;
+	for (int i = 0; i < 100; i++) {
+		int num = rand() % 1000;
+		sum = sum + num;
+		if (num > max)
+			max = num;
+	}
 }
